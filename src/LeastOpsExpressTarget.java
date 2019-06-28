9fa6c58a6c6740f74d2195ba622c1e4e20689dc3
qingweid <qingweid@mivatek.com>
2019-06-28 17:22:36 +0800

    Add Code

--- src/LeastOpsExpressTarget.java
+++ src/LeastOpsExpressTarget.java
@@ -1,5 +1,8 @@
 import com.sun.javafx.binding.StringFormatter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode-cn.com/problems/least-operators-to-express-number/
  * 给定一个正整数 x，我们将会写出一个形如 x (op1) x (op2) x (op3) x ... 的表达式，其中每个运算符 op1，op2，… 可以是加、减、乘、除（+，-，*，或是 /）之一。例如，对于 x = 3，我们可以写出表达式 3 * 3 / 3 + 3 - 3，该式的值为 3 。
@@ -23,51 +26,77 @@ import com.sun.javafx.binding.StringFormatter;
  */
 
 public class LeastOpsExpressTarget {
-    public int leastOpsExpressTarget(int x, int target) {
-        if (x == 1) return target - 1;
 
-        StringBuilder strBuilder = new StringBuilder();
-        int product;
-        while (true) {
-            if (target > x) {
-                product = 1;
-                for (int i = 1; i < 32; i++) {
-                    product *= x;
-                    if (product > target) {
-                        if (Math.abs(target - product / x) > Math.abs(target - product)){
-                            strBuilder.append("*");
-                            strBuilder.append(x);
-                            strBuilder.append("-");
-                            target = Math.abs(product - target);
-                        } else {
-                            target = Math.abs( target - product / x);
-                            strBuilder.append("+");
-                        }
-                        break;
-                    } else {
-                        //if (strBuilder.length() > 0 && i == 1) strBuilder.append("+");
-                        if (i > 1) strBuilder.append("*");
-                        strBuilder.append(x);
-                    }
-                }
-            } else if (target == x) {
-                if (strBuilder.length() > 0) {
-                    strBuilder.append("+");
-                }
-                strBuilder.append(x);
-                break;
-            }else if (target > 0) {
-//                if (strBuilder.length() > 0) {
-//                    strBuilder.append("+");
-//                }
-                strBuilder.append(x);
-                strBuilder.append("/");
-                strBuilder.append(x);
-                target -= 1;
-            } else break;
+    /**
+     * 算法思路：
+     *   如果 k1 = x^n; k2 = x^(n+1), 且 k1 < y < k2
+     *   则  有两种表示方式：
+     * pos the number of operations needed to get y % (x ^ (k+1))
+     * neg the number of operations needed to get x ^ (k + 1) - y % (x ^ (k + 1))
+     *
+     *
+     * 执行用时 :2 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗 :34 MB, 在所有 Java 提交中击败了100.00%的用户
+     */
+    public int leastOpsExpressTarget(int x, int y) {
+        int pos = 0, neg = 0, k = 0, pos2, neg2, cur;
+        while (y > 0) {
+            cur = y % x;
+            y /= x;
+            if (k > 0) {
+                pos2 = Math.min(cur * k + pos, (cur + 1) * k + neg);
+                neg2 = Math.min((x - cur) * k + pos, (x - cur - 1) * k + neg);
+                pos = pos2;
+                neg = neg2;
+            } else {
+                pos = cur * 2;
+                neg = (x - cur) * 2;
+            }
+            k++;
+        }
+        return Math.min(pos, k + neg) - 1;
+    }
+
+
+    /**
+     * 执行用时 :12 ms, 在所有 Java 提交中击败了50.00%的用户
+     * 内存消耗 :35.6 MB, 在所有 Java 提交中击败了80.00%的用户
+     */
+
+    /*
+    private Map<String, Integer> memo;
+    private int x;
+    public int leastOpsExpressTarget(int x, int target) {
+        memo = new HashMap<>();
+        this.x = x;
+        return dp(0, target) - 1;
+    }
+
+    private int dp(int i, int target) {
+        String code = "" + i + "#" + target;
+        if (memo.containsKey(code))
+            return memo.get(code);
+
+        int ans;
+        if (target == 0) {
+            ans = 0;
+        } else if (target == 1) {
+            ans = cost(i);
+        } else if (i >= 39) {
+            ans = target + 1;
+        } else {
+            int t = target / x;
+            int r = target % x;
+            ans = Math.min(r * cost(i) + dp(i+1, t),
+                    (x-r) * cost(i) + dp(i+1, t+1));
         }
 
-        System.out.println(strBuilder.toString());
-        return strBuilder.length() / 2 ;
+        memo.put(code, ans);
+        return ans;
     }
+
+    private int cost(int x) {
+        return x > 0 ? x : 2;
+    }
+    */
 }
